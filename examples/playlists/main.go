83b2e1cbec9eb20f79131929d1d7ee6934eba74f
einaralex <einaralex@gmail.com>
2026-02-14 00:34:09 +0000

    fix: playlist example, does not handle broken rekordbox references

--- examples/playlists/main.go
+++ examples/playlists/main.go
@@ -21,33 +21,42 @@ func main() {
 
 	client, err := rekordbox.NewClient(optionsFilePath)
 	if err != nil {
-		panic(err)
+	    panic(err)
 	}
 	defer client.Close()
 
 	// query all playlists
+	fmt.Println("DEBUG: Fetching all playlists...")
 	playlists, err := client.AllDjmdPlaylist(ctx)
 	if err != nil {
-		panic(err)
+	    panic(err)
 	}
 
-	for _, playlist := range playlists {
-		fmt.Printf("%s\n", playlist.Name.String())
+	fmt.Printf("DEBUG: Found %d playlists\n\n", len(playlists))
+
+	for i, playlist := range playlists {
+		fmt.Printf("DEBUG: [%d] Processing playlist: %s (ID: %s)\n", i+1, playlist.Name.String(), playlist.ID.String())
 
 		// query all songs that are in the playlist
 		playlistSongs, err := client.DjmdSongPlaylistByPlaylistID(ctx, playlist.ID)
 		if err != nil {
-			panic(err)
+			fmt.Printf("DEBUG: ERROR fetching songs for playlist %s: %v\n", playlist.Name.String(), err)
+			continue
 		}
+
+		fmt.Printf("DEBUG: Found %d songs in playlist\n", len(playlistSongs))
 
 		// get all songs within the playlist
 		for _, playlistSong := range playlistSongs {
 			content, err := client.DjmdContentByID(ctx, playlistSong.ContentID)
 			if err != nil {
-				panic(err)
+				fmt.Printf("DEBUG: ERROR fetching content %s: %v\n", playlistSong.ContentID.String(), err)
+				continue
 			}
 
 			fmt.Printf("\t%s\n", content.Title.String())
 		}
+		fmt.Println()
 	}
+
 }
